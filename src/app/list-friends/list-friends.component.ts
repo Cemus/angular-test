4c6200c229f641128924dc88f6481f1c5cc5f493
Cemus <24947582+Cemus@users.noreply.github.com>
2025-02-10 15:14:40 +0100

    Test ngfor

--- src/app/list-friends/list-friends.component.ts
+++ src/app/list-friends/list-friends.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
 import { OneFriendComponent } from '../one-friend/one-friend.component';
+import { Friend } from '../models/friend.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list-friends',
-  imports: [OneFriendComponent],
+  imports: [CommonModule, OneFriendComponent],
   templateUrl: './list-friends.component.html',
   styleUrl: './list-friends.component.css',
 })
-export class ListFriendsComponent {}
+export class ListFriendsComponent {
+  randomized(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  friends: Friend[] = [
+    {
+      name: 'Jean',
+      status: 'En ligne',
+      bio: 'Blablablablablabla',
+      passions: ['Ballon rond', 'Jeu de rôle japonais', 'La lessive'],
+      reputation: 'Cool',
+      age: (): string => {
+        const random = this.randomized(19, 58);
+        return `${random.toString()} ans`;
+      },
+    },
+    {
+      name: 'Vanessa',
+      status: 'Hors ligne',
+      bio: 'Blablablablablabla',
+      passions: ['Les chiens', 'Les chats', 'Les rats'],
+      reputation: 'Sereine',
+      age: (): string => {
+        const random = this.randomized(19, 58);
+        return `${random.toString()} ans`;
+      },
+    },
+    {
+      name: 'Paul',
+      status: 'Hors ligne',
+      bio: 'Blablablablablabla',
+      passions: ['La tarte', 'Les bardes', 'Les barques'],
+      reputation: 'Dur',
+      age: (): string => {
+        const random = this.randomized(19, 58);
+        return `${random.toString()} ans`;
+      },
+    },
+  ];
+}
--- src/app/one-friend/one-friend.component.ts
+++ src/app/one-friend/one-friend.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Friend } from '../models/friend.interface';
 import { CommonModule } from '@angular/common';
 
@@ -9,15 +9,5 @@ import { CommonModule } from '@angular/common';
   styleUrl: './one-friend.component.css',
 })
 export class OneFriendComponent {
-  friend: Friend = {
-    name: 'Jean',
-    status: 'En ligne',
-    bio: 'Blablablablablabla',
-    passions: ['Ballon rond', 'Jeu de rôle japonais', 'La lessive'],
-    reputation: 'cool',
-    age: (): string => {
-      const random = Math.floor(Math.random() * 3);
-      return random > 1 ? '50 balais' : '19 ans';
-    },
-  };
+  @Input() friend!: Friend;
 }
