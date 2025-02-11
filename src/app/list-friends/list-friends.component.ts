3499572cd2a682a96316fda11ffb799adf60efa3
Cemus <24947582+Cemus@users.noreply.github.com>
2025-02-11 14:07:35 +0100

    Fix CommonModule import

--- src/app/list-friends/list-friends.component.ts
+++ src/app/list-friends/list-friends.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FriendsService } from '../friends.service'; // Import du service
-import { Friend } from '../models/friend.interface'; // Import du type Friend
 import { OneFriendComponent } from '../one-friend/one-friend.component';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-list-friends',
   standalone: true,
-  imports: [CommonModule, OneFriendComponent, FormsModule],
+  imports: [NgIf, OneFriendComponent, FormsModule],
   templateUrl: './list-friends.component.html',
   styleUrls: ['./list-friends.component.css'],
 })
