0f529e91e59ee9e03a12c34a923289e01f1f0415
Cemus <24947582+Cemus@users.noreply.github.com>
2025-02-12 21:52:41 +0100

    Test

--- launch.sh
+++ launch.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-if [ !$1 ]
+read -p 'Entrez un commentaire : ' commentaire
+
+if [ -z "$commentaire" ]
 then
-echo "Pas de commentaire GitHub le low";
-exit 1;
-else
+    echo "Pas de commentaire GitHub le low";
+    exit 1;
+fi
+
 git add .
-git commit -m "$1"
+git commit -m "$commentaire"
+
+if [ $? -ne 0 ]
+then
+    echo "Erreur lors du commit, annulation du push."
+    exit 1
+fi
+
 git push
-npm run gbd
+
+if [ $? -eq 0 ]
+then
+    npm run gbd
+else
+    echo "Échec du push, build et déploiement non exécutés."
+    exit 1
 fi
