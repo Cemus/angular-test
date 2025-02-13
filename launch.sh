22cf5dac90a3eddd9742d0b00cb6841055f6947e
Cemus <24947582+Cemus@users.noreply.github.com>
2025-02-13 09:26:07 +0100

    Create services

--- launch.sh
+++ launch.sh
@@ -2,11 +2,11 @@
 
 if [ !$1 ]
 then
-echo "Pas de commentaire GitHub le low";
-exit 1;
+    echo "Pas de commentaire GitHub le low";
+    exit 1;
 else
-git add .
-git commit -m "$1"
-git push
-npm run gbd
+    git add .
+    git commit -m "$1"
+    git push
+    npm run gbd
 fi
